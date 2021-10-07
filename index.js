57eee4210d94b9685020f4c27cdfec061e671728
chouhanviky9 <chouhanviky9@gmail.com>
2021-10-07 18:38:32 +0530

    5

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.on("error", function(error) {
     console.error(error);
   });
 
-  
+
   client.set("name","vikas");
   client.get("name",redis.print);
   client.exists("chouhanvi",redis.print);
@@ -23,7 +23,21 @@ client.on("error", function(error) {
   client.incr("chouhanviky9",redis.print);
   client.del("name",redis.print);
   client.get("name",redis.print);
+//   client.lpush("map","one");
 
+  client.lrange("map",0,-1,redis.print);
+  client.hset("hashset","nook","kiiaa",redis.print);
+  client.hdel("hashset","nook","kiiaa");
+  client.hexists("hashset","name",redis.print);
+  client.hgetall("hashset",(obj,err)=>{
+      if(err){
+          console.log(err);
+      }
+      console.log(obj);
+  });
+  client.llen("map",redis.print);
+  client.lindex("map",5,redis.print);
+  client.ltrim("map",0,1,redis.print);
 
 
 
