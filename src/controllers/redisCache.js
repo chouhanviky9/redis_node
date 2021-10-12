4a6bd0d308e9624e8747dfc8f5b14777510cc363
chouhanviky9 <chouhanviky9@gmail.com>
2021-10-12 16:54:23 +0530

    added

--- src/controllers/redisCache.js
+++ src/controllers/redisCache.js
@@ -50,10 +50,28 @@ admin.getSet = async (req, res, next) => {
         res.status(500).send(err);
     }
 };
-admin.type =async (req, res, next) => {
+admin.Type =async (req, res, next) => {
     try {
         let response = await cache.type(req.query.key);
         res.send(response);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+admin.Rename =async (req, res, next) => {
+    try {
+        let response = await cache.rename(req.query.key,req.query.newKey);
+        res.send(response);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+admin.Dump =async (req, res, next) => {
+    try {
+        let response = await cache.dumpCache(req.query.key);
+        res.send(`${response}`);
     } catch (err) {
         res.status(500).send(err);
     }
@@ -72,6 +90,49 @@ admin.getMany = async (req, res, next) => {
     try {
         let response = await cache.getMany(req.query.keys);
         res.send(response);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+admin.renameNX = async (req, res, next) => {
+    try {
+        let response = await cache.renameNX(req.query.key, req.query.newKey);
+        res.send(`${response}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+admin.isExist = async (req, res, next) => {
+    try {
+        let response = await cache.isCache(req.query.key);
+        res.send(`${response}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+admin.Expire = async (req, res, next) => {
+    try {
+        let response = await cache.expire(req.query.key,req.query.time);
+        res.send(`${response}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+admin.ExpireTimeLeft = async (req, res, next) => {
+    try {
+        let response = await cache.getExpireTime(req.query.key);
+        res.send(`${response}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+admin.RandomCache = async (req, res, next) => {
+    try {
+        let response = await cache.randomCache();
+        res.send(`${response}`);
     } catch (err) {
         res.status(500).send(err);
     }
@@ -264,6 +325,105 @@ admin.SRANDOM = async (req, res, next) => {
 admin.SUNIONSTORE = async (req, res, next) => {
     try {
         let response = await cache.SUNIONSTORE(req.query.names, req.query.name1, req.query.name2);
+        res.send(`${response}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+//zset operations 
+admin.zADD = async (req, res, next) => {
+    try {
+        let response = await cache.zADD(req.query.name, req.query.score, req.query.value);
+        res.send(`${response}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
+
+admin.zCard = async (req, res, next) => {
+    try {
+        let response = await cache.zCard(req.query.name);
+        res.send(`${response}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
+admin.zCount = async (req, res, next) => {
+    try {
+        let response = await cache.zCount(req.query.name,req.query.start,req.query.end);
+        res.send(`${response}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
+
+admin.zINCRBY = async (req, res, next) => {
+    try {
+        let response = await cache.ZINCRBY(req.query.name,req.query.incr,req.query.value);
+        res.send(`${response}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
+
+admin.zScore = async (req, res, next) => {
+    try {
+        let response = await cache.getZScore(req.query.name,req.query.value);
+        res.send(`${response}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
+
+admin.ZRange = async (req, res, next) => {
+    try {
+        let response = await cache.ZRange(req.query.name, req.query.start, req.query.end);
+        res.send(`${response}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+admin.ZRangeByScore = async (req, res, next) => {
+    try {
+        let response = await cache.ZRangeByScore(req.query.name, req.query.min, req.query.max);
+        res.send(`${response}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+admin.ZRank = async (req, res, next) => {
+    try {
+        let response = await cache.ZRank(req.query.name,req.query.value);
+        res.send(`${response}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+admin.ZRem = async (req, res, next) => {
+    try {
+        let response = await cache.ZRem(req.query.name,req.query.value);
+        res.send(`${response}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+admin.ZRemRangeByScore = async (req, res, next) => {
+    try {
+        let response = await cache.ZRemoveRangeByScore(req.query.name,req.query.min,req.query.max);
+        res.send(`${response}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+admin.ZRemRangeByIndex = async (req, res, next) => {
+    try {
+        let response = await cache.ZRemoveRangeByIndex(req.query.name,req.query.start,req.query.end);
         res.send(`${response}`);
     } catch (err) {
         res.status(500).send(err);
--- src/services/cache.js
+++ src/services/cache.js
@@ -3,335 +3,533 @@ const redis = require("redis");
 
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 const client = redis.createClient({
-    host : '127.0.0.1',  
-          no_ready_check: true,
-          auth_pass: '786Hh@786',  
+    host: "127.0.0.1",
+    no_ready_check: true,
+    auth_pass: "786Hh@786"
 });
 
-let cache={};
+let cache = {};
 
-// operations normal string data type 
+// operations normal string data type
 //operations on strings and number
 
-cache.setCache=(key,value)=>{
-    return new Promise((resolve,reject)=>{
-        client.set(key,value,(err,reply)=>{
-            if(err){
+cache.setCache = (key, value) => {
+    return new Promise((resolve, reject) => {
+        client.set(key, value, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.getCache=(key)=>{
-    return new Promise((resolve,reject)=>{
-        client.get(key,(err,reply)=>{
-            if(err){
+cache.getCache = (key) => {
+    return new Promise((resolve, reject) => {
+        client.get(key, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.deleteCache=(key)=>{
-    return new Promise((resolve,reject)=>{
-        client.del(key,(err,reply)=>{
-            if(err){
+cache.deleteCache = (key) => {
+    return new Promise((resolve, reject) => {
+        client.del(key, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.RangeCache=(key,start,end)=>{
-    return new Promise((resolve,reject)=>{
-        client.GETRANGE(key,start,end,(err,reply)=>{
-            if(err){
+cache.RangeCache = (key, start, end) => {
+    return new Promise((resolve, reject) => {
+        client.GETRANGE(key, start, end, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.getSet=(key,value)=>{
-    return new Promise((resolve,reject)=>{
-        client.GETSET(key,value,(err,reply)=>{
-            if(err){
+cache.getSet = (key, value) => {
+    return new Promise((resolve, reject) => {
+        client.GETSET(key, value, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.getBitAt=(key,offset)=>{
-    return new Promise((resolve,reject)=>{
-        client.GETBIT(key,offset,(err,reply)=>{
-            if(err){
+cache.getBitAt = (key, offset) => {
+    return new Promise((resolve, reject) => {
+        client.GETBIT(key, offset, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
-cache.getMany=(keys)=>{
-    return new Promise((resolve,reject)=>{
-        client.MGET(...keys,(err,reply)=>{
-            if(err){
+        });
+    });
+};
+cache.getMany = (keys) => {
+    return new Promise((resolve, reject) => {
+        client.MGET(...keys, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.type=(key)=>{
-    return new Promise((resolve,reject)=>{
-        client.type(key,(err,reply)=>{
-            if(err){
+cache.type = (key) => {
+    return new Promise((resolve, reject) => {
+        client.type(key, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
+
+cache.rename = (key, newKey) => {
+    return new Promise((resolve, reject) => {
+        client.RENAME(key, newKey, (err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+};
+
+cache.renameNX = (key, newKey) => {
+    return new Promise((resolve, reject) => {
+        client.RENAMENX(key, newKey, (err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+};
+
+cache.dumpCache = (key) => {
+    return new Promise((resolve, reject) => {
+        client.dump(key,(err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+};
+
+cache.isCache = (key) => {
+    return new Promise((resolve, reject) => {
+        client.exists(key,(err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+};
+
+cache.expire = (key,time) => {
+    return new Promise((resolve, reject) => {
+        client.expire(key,time,(err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+};
+
+cache.getExpireTime = (key,time) => {
+    return new Promise((resolve, reject) => {
+        client.TTL(key,(err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+};
+
+cache.randomCache = (key,time) => {
+    return new Promise((resolve, reject) => {
+        client.RANDOMKEY((err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+};
 //redis list
-cache.lPush= (name,value) => {
-    return new Promise((resolve,reject)=>{
-        client.lpush(name,value,(err,reply)=>{
-            if(err){
+cache.lPush = (name, value) => {
+    return new Promise((resolve, reject) => {
+        client.lpush(name, value, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.lSet=(name,key,value)=>{
-    return new Promise((resolve,reject)=>{
-        client.lset(name,key,value,(err,reply)=>{
-            if(err){
+cache.lSet = (name, key, value) => {
+    return new Promise((resolve, reject) => {
+        client.lset(name, key, value, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
-cache.lPushX= (name,value) => {
-    return new Promise((resolve,reject)=>{
-        client.lpushx(name,value,(err,reply)=>{
-            if(err){
+        });
+    });
+};
+cache.lPushX = (name, value) => {
+    return new Promise((resolve, reject) => {
+        client.lpushx(name, value, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.rPush= (name,value) => {
-    return new Promise((resolve,reject)=>{
-        client.rpush(name,value,(err,reply)=>{
-            if(err){
+cache.rPush = (name, value) => {
+    return new Promise((resolve, reject) => {
+        client.rpush(name, value, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.rPushX= (name,value) => {
-    return new Promise((resolve,reject)=>{
-        client.rpushx(name,value,(err,reply)=>{
-            if(err){
+cache.rPushX = (name, value) => {
+    return new Promise((resolve, reject) => {
+        client.rpushx(name, value, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.lRange= (name,start,end) => {
-    return new Promise((resolve,reject)=>{
-        client.lrange(name,start,end,(err,reply)=>{
-            if(err){
+cache.lRange = (name, start, end) => {
+    return new Promise((resolve, reject) => {
+        client.lrange(name, start, end, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.lRemove= (name,count,value) => {
-    return new Promise((resolve,reject)=>{
-        client.lrem(name,count,value,(err,reply)=>{
-            if(err){
+cache.lRemove = (name, count, value) => {
+    return new Promise((resolve, reject) => {
+        client.lrem(name, count, value, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.lPop= (name) => {
-    return new Promise((resolve,reject)=>{
-        client.lpop(name,(err,reply)=>{
-            if(err){
+cache.lPop = (name) => {
+    return new Promise((resolve, reject) => {
+        client.lpop(name, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
-//redis hashes 
+        });
+    });
+};
+//redis hashes
 
-cache.hSet=(name,key,value)=>{
-    return new Promise((resolve,reject)=>{
-        client.HSET(name,key,value,(err,reply)=>{
-            if(err){
+cache.hSet = (name, key, value) => {
+    return new Promise((resolve, reject) => {
+        client.HSET(name, key, value, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
-cache.hGet=(name,key,value)=>{
-    return new Promise((resolve,reject)=>{
-        client.HGET(name,key,(err,reply)=>{
-            if(err){
+        });
+    });
+};
+cache.hGet = (name, key, value) => {
+    return new Promise((resolve, reject) => {
+        client.HGET(name, key, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.hDel=(name,key)=>{
-    return new Promise((resolve,reject)=>{
-        client.HDEL(name,key,(err,reply)=>{
-            if(err){
+cache.hDel = (name, key) => {
+    return new Promise((resolve, reject) => {
+        client.HDEL(name, key, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.hVals=(name)=>{
-    return new Promise((resolve,reject)=>{
-        client.HVALS(name,(err,reply)=>{
-            if(err){
+cache.hVals = (name) => {
+    return new Promise((resolve, reject) => {
+        client.HVALS(name, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
 //operations on sets
 
-cache.SADD=(name,value)=>{
-    return new Promise((resolve,reject)=>{
-        client.SADD(name,value,(err,reply)=>{
-            if(err){
+cache.SADD = (name, value) => {
+    return new Promise((resolve, reject) => {
+        client.SADD(name, value, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.SREM=(name,value)=>{
-    return new Promise((resolve,reject)=>{
-        client.SREM(name,value,(err,reply)=>{
-            if(err){
+cache.SREM = (name, value) => {
+    return new Promise((resolve, reject) => {
+        client.SREM(name, value, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.SMembers=(name)=>{
-    return new Promise((resolve,reject)=>{
-        client.SMEMBERS(name,(err,reply)=>{
-            if(err){
+cache.SMembers = (name) => {
+    return new Promise((resolve, reject) => {
+        client.SMEMBERS(name, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.SCARD=(name)=>{
-    return new Promise((resolve,reject)=>{
-        client.SCARD(name,(err,reply)=>{
-            if(err){
+cache.SCARD = (name) => {
+    return new Promise((resolve, reject) => {
+        client.SCARD(name, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.SDIFF=(a,b)=>{
-    return new Promise((resolve,reject)=>{
-        client.SDIFF(a,b,(err,reply)=>{
-            if(err){
+cache.SDIFF = (a, b) => {
+    return new Promise((resolve, reject) => {
+        client.SDIFF(a, b, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.SINTER=(a,b)=>{
-    return new Promise((resolve,reject)=>{
-        client.SINTER(a,b,(err,reply)=>{
-            if(err){
+cache.SINTER = (a, b) => {
+    return new Promise((resolve, reject) => {
+        client.SINTER(a, b, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.SISMEMBER=(name,key)=>{
-    return new Promise((resolve,reject)=>{
-        client.SISMEMBER(name,key,(err,reply)=>{
-            if(err){
+cache.SISMEMBER = (name, key) => {
+    return new Promise((resolve, reject) => {
+        client.SISMEMBER(name, key, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.SRANDOM=(name)=>{
-    return new Promise((resolve,reject)=>{
-        client.SRANDMEMBER(name,(err,reply)=>{
-            if(err){
+cache.SRANDOM = (name) => {
+    return new Promise((resolve, reject) => {
+        client.SRANDMEMBER(name, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
-}
+        });
+    });
+};
 
-cache.SUNIONSTORE=(name,name1,name2)=>{
-    return new Promise((resolve,reject)=>{
-        client.SUNIONSTORE(name,name1,name2,(err,reply)=>{
-            if(err){
+cache.SUNIONSTORE = (name, name1, name2) => {
+    return new Promise((resolve, reject) => {
+        client.SUNIONSTORE(name, name1, name2, (err, reply) => {
+            if (err) {
                 resolve(err);
             }
             resolve(reply);
-        })
-    })
+        });
+    });
+};
+
+//zset
+
+cache.zADD=(name,score,value)=>{
+    return new Promise((resolve, reject) => {
+        client.zadd(name, score, value, (err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
 }
 
+cache.zCard=(name)=>{
+    return new Promise((resolve, reject) => {
+        client.zcard(name ,(err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+}
 
+cache.zCount=(name,start,end)=>{
+    return new Promise((resolve, reject) => {
+        client.zcount(name,start,end,(err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+}
 
-module.exports =cache;
+cache.ZINCRBY=(name,incr,value)=>{
+    return new Promise((resolve, reject) => {
+        client.ZINCRBY(name,incr,value,(err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+}
+
+cache.getZScore=(name,value)=>{
+    return new Promise((resolve, reject) => {
+        client.ZSCORE(name,value,(err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+}
+
+cache.ZRange = (name, start, end) => {
+    return new Promise((resolve, reject) => {
+        client.zrange(name, start, end, (err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+};
+
+cache.ZRangeByScore = (name, min, max) => {
+    return new Promise((resolve, reject) => {
+        client.zrangebyscore(name, min, max, (err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+};
+
+cache.ZRank = (name,value) => {
+    return new Promise((resolve, reject) => {
+        client.zrank(name, value, (err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+};
+
+cache.ZRem = (name,value) => {
+    return new Promise((resolve, reject) => {
+        client.zrem(name, value, (err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+};
+
+cache.ZRemoveRangeByScore = (name,min,max) => {
+    return new Promise((resolve, reject) => {
+        client.ZREMRANGEBYSCORE(name,min,max, (err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+};
+
+cache.ZRemoveRangeByIndex = (name,start,end) => {
+    return new Promise((resolve, reject) => {
+        client.ZREMRANGEBYRANK(name,start,end, (err, reply) => {
+            if (err) {
+                resolve(err);
+            }
+            resolve(reply);
+        });
+    });
+};
+
+module.exports = cache;
